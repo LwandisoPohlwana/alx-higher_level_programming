86e83e1fe98a6c09ced8c619b1a8fbf76ed951c8
Lwandiso Pohlwana <85118790+LwandisoPohlwana@users.noreply.github.com>
2023-08-11 08:04:13 +0200

    Update 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,9 @@
-!#/usr/bun/python3
+!#/usr/bin/python3
 
-if_name_=="_main_":
+if __name__ == "__main__":
     """print the sum of 1 and 2."""
     from add_0 import add
 
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, add(a,b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
