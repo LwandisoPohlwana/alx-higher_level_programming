6f2ab7631597e74829c5d650a3941e0636a8c487
lwandisoPohlwana <lwandisopohlwana.com@gmail.com>
2023-08-14 07:33:32 -0700

    fix

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -3,7 +3,7 @@
 def divisible_by_2(my_list=[]):
     """Find all multiples of 2 in a list."""
     multiple = []
-    for i in range(len(my_list)):
+    for v in range(len(my_list)):
         if my_list[v] % 2 == 0:
             multiple.append(True)
         else:
--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -5,6 +5,6 @@ def new_in_list(my_list, idx, element):
     if idx < 0 or idx > (len(my_list)-1):
         return (my_list)
 
-    lst_copy = [x for x my_list]
+    lst_copy = [x for x in my_list]
     lst_copy = element
     return (lst_copy)
